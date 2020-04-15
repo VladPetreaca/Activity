cfb256352cae89869082635b05eb363255902723
VladPetreaca <vlad.2898@yahoo.com>
2020-04-15 21:49:04 +0300

    VladPetreaca

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
 
+		hideNavigationBar();
+
 		setting_btn = (ImageButton) findViewById(R.id.button2);
 		info_btn = (ImageButton) findViewById(R.id.imageButton2);
 
@@ -30,25 +35,44 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View v) {
 				Toast.makeText(MainActivity.this, "Setting Button Clicked.", Toast.LENGTH_SHORT).show();
-				openSettings();
+				//openSettings();
 			}
 		});
 
 		info_btn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//Toast.makeText(MainActivity.this, "The Rules.", Toast.LENGTH_SHORT).show();
 				openHelp();
 			}
 		});
 
 	}
-	public void openSettings() {
-		Intent intent = new Intent(this, Settings.class);
-		startActivity(intent);
+
+	public void onResume() {
+		super.onResume();
+
+		hideNavigationBar();
 	}
+
+	public void hideNavigationBar() {
+		this.getWindow().getDecorView()
+				.setSystemUiVisibility(
+						View.SYSTEM_UI_FLAG_FULLSCREEN |
+								View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+								View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+								View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+								View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+								View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				);
+	}
+
+//	public void openSettings() {
+//		Intent intent = new Intent(this, Settings.class);
+//		startActivity(intent);
+//	}
 	public void openHelp() {
 		Intent intent = new Intent(this, Help.class);
 		startActivity(intent);
+
 	}
 }
